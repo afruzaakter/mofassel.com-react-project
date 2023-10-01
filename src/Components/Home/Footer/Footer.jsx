112879cef75b18a975a6be47cd76123d0390a9c6
afruzaakter <afruzaaktersonia1995@gmail.com>
2023-10-01 19:53:17 +0600

    footer problem solve

--- src/Components/Home/Footer/Footer.jsx
+++ src/Components/Home/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <div className="bg-primary lg:w-screen  md:w-screen w-screen p-3  flex justify-center items-center">
+        <div className="bg-primary  p-3  flex justify-center items-center">
             <p className=" text-black text-xl p-14 ">© Copyright 2018-2025 | MOFASSEL.COM</p>
         </div>
     );
