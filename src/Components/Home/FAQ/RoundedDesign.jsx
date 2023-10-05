3460d1790cded7412ddd2b295cf0814b05b3a086
afruzaakter <afruzaaktersonia1995@gmail.com>
2023-10-05 18:14:57 +0600

    google font problem solve

---
+++ src/Components/Home/FAQ/RoundedDesign.jsx
@@ -0,0 +1,13 @@
+
+
+const RoundedDesign = () => {
+    return (
+        <div className="mr-48 ">
+            <div className="w-48 h-48 bg-[#FF9C00]  rounded-full">
+              <h1></h1>
+            </div>
+        </div>
+    );
+};
+
+export default RoundedDesign;
